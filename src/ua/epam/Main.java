fd08392e798906810dc54afceae7ce4db6bf6e48
Dmytro Rybin <dmytro_rybin@epam.com>
2016-09-25 19:33:07 +0300

    relations code added

---
+++ src/ua/epam/Main.java
@@ -0,0 +1,17 @@
+package ua.epam;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Operator operator = new Operator("Jack", "Jonsons");
+        Robot robot = new Robot(20, 30);
+
+        operator.setRobot(robot);
+        System.out.println(operator.getRobot().getCourse());
+
+        robot.setOperator(operator);
+        robot.forward(30);
+        robot.printCoordinates();
+        System.out.println(robot.getOperator().getFirstName());
+    }
+}
